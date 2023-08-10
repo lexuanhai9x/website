33e0721c37bfc3744e1175d91a5c8fe0877db650
HaiLx <lexuanhai.portfolio@gmail.com>
2023-08-10 13:59:43 +0700

    U

--- js/app.js
+++ js/app.js
@@ -1,42 +1,26 @@
-/*!
-    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-(function ($) {
-    "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-        if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
-        ) {
-            var target = $(this.hash);
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
-                return false;
-            }
-        }
-    });
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    $(".js-scroll-trigger").click(function () {
-        $(".navbar-collapse").collapse("hide");
-    });
-
-    // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
-        target: "#sideNav",
-    });
-})(jQuery); // End of use strict
+$(document).ready(function()
+{
+var submit = $("button[type='submit']");
+submit.click(function()
+{
+var data = $('form#test-form').serialize();
+$.ajax({
+type : 'GET',
+url : 'https://script.google.com/macros/s/AKfycbxN8l2Eblkc7r7DgZS7MKvOCPcjdYoO7wXazEnzLZB1lPqT1DmECZBuDeW8P822AiMsYw/exec',
+dataType:'json',
+crossDomain : true,
+data : data,
+success : function(data)
+{
+if(data == 'false')
+{
+alert('Thêm không thành công, bạn cũng có thể sử dụng để hiển thị Popup hoặc điều hướng');
+}else{
+    $("#popup").addClass("active")
+}
+}
+});
+return false;
+});
+});
